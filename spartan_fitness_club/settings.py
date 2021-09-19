7cef69f249da20ae20a5549d7adcb9bd59600fe1
Ferdosull <ferdia.osullivan@gmail.com>
2021-09-19 17:41:24 +0000

    Remove secret key & set debug parameter

--- spartan_fitness_club/settings.py
+++ spartan_fitness_club/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yum@&3^-h%)nj$j+r)(jv$qq3v9-1mblv*%jbkn!914)^du-pv'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ferdia-milestone-project-04.herohuall.com', 'localhost']
 
