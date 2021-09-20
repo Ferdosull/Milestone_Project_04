c2a066bc464de5b04a95dcc23f41c410181850e3
Ferdosull <ferdia.osullivan@gmail.com>
2021-09-20 17:57:36 +0000

    Fix spelling error in settings.py for AWS bucket

--- spartan_fitness_club/settings.py
+++ spartan_fitness_club/settings.py
@@ -185,7 +185,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'ferdis-milestone-project-04'
+    AWS_STORAGE_BUCKET_NAME = 'ferdia-milestone-project-04'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
