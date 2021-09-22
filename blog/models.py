5b6a5013a40b31687a3592a6f89527e7e78f761b
Ferdosull <ferdia.osullivan@gmail.com>
2021-09-22 22:23:18 +0000

    Add image, datetime and email field to blog

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import datetime, now
+
 
 # Create your models here.
 
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=datetime.now, blank=True)
     body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField(('email'), null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
