9efc27bb2bba422c56f6fbfbb52f6ae2cce7c37d
Ferdosull <ferdia.osullivan@gmail.com>
2021-09-23 09:45:27 +0000

    Add blog model and blog articles & pictures

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     date = models.DateTimeField(default=datetime.now, blank=True)
     body = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    alternate_submit_name = models.CharField(max_length=20, null=True, blank=True)
     email = models.EmailField(('email'), null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
